358bfc01bf911d664c1e90a6cb22c0f63fde0fa0
Steven Ongkowidjojo <songkowidjojo@student.ciputra.ac.id>
2024-05-24 12:04:37 +0700

    add wallpaper image

--- basketballGame/mainMenu.swift
+++ basketballGame/mainMenu.swift
@@ -9,80 +9,77 @@ import SwiftUI
 
 struct mainMenu: View {
     var body: some View {
-        ZStack {
-            Image("Wallpaper")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .edgesIgnoringSafeArea(.all)
-            
-            Color.black.opacity(0.7)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                Spacer().frame(height: 70)
-                Text("AR HOOPS")
-                    .font(.custom("SpongeboyMeBob", size: 55))
-                    .foregroundColor(.white)
-                Spacer().frame(height: 280) // Menambahkan spacer untuk memposisikan konten lainnya di bawah logo
-                // Tombol pertama: Solo
-                VStack {
-                    Button(action: {
-                        // Aksi untuk tombol Solo
-                    }) {
-                        Text("Solo")
-                        //                            .font(.title)
-                        //                            .fontWeight(.bold)
-                            .foregroundColor(.myCustomColor1)
-                            .padding()
-                            .frame(maxWidth: 290)
-                            .background(Color.myCustomColor2)
-                            .cornerRadius(10)
-                            .padding(.horizontal)
-                            .font(.custom("RichuMastRegular", size: 30))
-                    }
-                    
-                    Spacer().frame(height: 20) // Spacer untuk memberi jarak antar tombol
-                    
-                    // Tombol kedua: Multi Player
-                    Button(action: {
-                        // Aksi untuk tombol Multi Player
-                    }) {
-                        Text("MultiPlayer")
-                        //                            .font(.title)
-                        //                            .fontWeight(.bold)
-                            .foregroundColor(.myCustomColor1)
-                            .padding()
-                            .frame(maxWidth: 290)
-                            .background(Color.myCustomColor2)
-                            .cornerRadius(10)
-                            .padding(.horizontal)
-                            .font(.custom("RichuMastRegular", size: 30))
-                    }
-                    
-                    Spacer().frame(height: 20) // Spacer untuk memberi jarak antar tombol
-                    
-                    // Tombol ketiga: Leader Board
-                    Button(action: {
-                        // Aksi untuk tombol Leader Board
-                    }) {
-                        Text("LeaderBoard")
-                        //                            .font(.title)
-                        //                            .fontWeight(.bold)
-                            .foregroundColor(.myCustomColor1)
-                            .padding()
-                            .frame(maxWidth: 290)
-                            .background(Color.myCustomColor2)
-                            .cornerRadius(10)
-                            .padding(.horizontal)
-                            .font(.custom("RichuMastRegular", size: 30))
-                    }
-                }
-                .offset(y: -125)
+        NavigationView {
+            ZStack {
+                Image("Wallpaper")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .edgesIgnoringSafeArea(.all)
                 
+                Color.black.opacity(0.7)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Spacer().frame(height: 70)
+                    Text("AR HOOPS")
+                        .font(.custom("SpongeboyMeBob", size: 55))
+                        .foregroundColor(.white)
+                    Spacer().frame(height: 280) // Menambahkan spacer untuk memposisikan konten lainnya di bawah logo
+                    // Tombol pertama: Solo
+                    VStack {
+                        NavigationLink(destination: ContentView()) {
+                            Text("Solo Player")
+                        }
+                        .foregroundColor(.myCustomColor1)
+                        .padding()
+                        .frame(maxWidth: 290)
+                        .background(Color.myCustomColor2)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                        .font(.custom("RichuMastRegular", size: 30))
+                        Spacer().frame(height: 20) // Spacer untuk memberi jarak antar tombol
+                        
+                        // Tombol kedua: Multi Player
+                        Button(action: {
+                            // Aksi untuk tombol Multi Player
+                        }) {
+                            Text("MultiPlayer")
+                            //                            .font(.title)
+                            //                            .fontWeight(.bold)
+                                .foregroundColor(.myCustomColor1)
+                                .padding()
+                                .frame(maxWidth: 290)
+                                .background(Color.myCustomColor2)
+                                .cornerRadius(10)
+                                .padding(.horizontal)
+                                .font(.custom("RichuMastRegular", size: 30))
+                        }
+                        
+                        Spacer().frame(height: 20) // Spacer untuk memberi jarak antar tombol
+                        
+                        // Tombol ketiga: Leader Board
+                        Button(action: {
+                            // Aksi untuk tombol Leader Board
+                        }) {
+                            Text("LeaderBoard")
+                            //                            .font(.title)
+                            //                            .fontWeight(.bold)
+                                .foregroundColor(.myCustomColor1)
+                                .padding()
+                                .frame(maxWidth: 290)
+                                .background(Color.myCustomColor2)
+                                .cornerRadius(10)
+                                .padding(.horizontal)
+                                .font(.custom("RichuMastRegular", size: 30))
+                        }
+                    }
+                    .offset(y: -125)
+                    
+                }
+                
+                .padding(.top, 50) // Tambahkan padding jika ingin memberikan jarak dari atas layar
             }
-            
-            .padding(.top, 50) // Tambahkan padding jika ingin memberikan jarak dari atas layar
         }
     }
 }
